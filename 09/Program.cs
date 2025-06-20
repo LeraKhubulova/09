fec6bba28bce2a493ecd33157138a35dc933715d
Vadim1753 <vadimbaimatov4@gmail.com>
2025-06-20 11:23:24 +0300

    ответ

--- 09/Program.cs
+++ 09/Program.cs
@@ -1,17 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 
-namespace _09
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Введите текст на английском (заканчивается точкой):");
+        string text = Console.ReadLine().ToLower();
+         string cleanText = "";
+        foreach (char c in text)
         {
-           
+            if (char.IsLetter(c) || c == '.')
+                cleanText += c;
         }
+        if (cleanText.Length == 0 || cleanText == ".")
+        {
+             Console.WriteLine("В тексте нет букв!");
+            return;
+        }
+        int[] counts = new int[26];
+         foreach (char c in cleanText)
+        {
+            if (char.IsLetter(c))
+            {
+                int index = c - 'a'; 
+                counts[index]++;
+            }
+        }
+        char maxChar = 'a';
+         int maxCount = 0;
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (counts[i] > maxCount)
+            {
+                maxCount = counts[i];
+                maxChar= (char)('a' + i);
+            }
+        }
+
+        Console.WriteLine($"Самая частая буква: '{maxChar}'");
+        Console.WriteLine($"Количество: {maxCount}");
     }
 }
